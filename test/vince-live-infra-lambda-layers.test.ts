183aa9917efd7718adb54c90410cfccb5aa3dcf8
Janne Kinghed <janne.kinghed@vince.no>
2021-12-08 13:59:52 +0100

    Initial commit

---
+++ test/vince-live-infra-lambda-layers.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as VinceLiveInfraLambdaLayers from '../lib/vince-live-infra-lambda-layers-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/vince-live-infra-lambda-layers-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new VinceLiveInfraLambdaLayers.VinceLiveInfraLambdaLayersStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
